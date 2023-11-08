819d9827365835d3711c01169b4411f596d91f8c
AsRain <144534872+AsRain@users.noreply.github.com>
2023-11-08 15:12:52 +0900

    Add files via upload

--- feature/chat/chat.js
+++ feature/chat/chat.js
@@ -1,56 +1,33 @@
-var CHAT = CHAT || {};
+const socket = new WebSocket('ws://your-server-url'); // WebSocketサーバーのURLに置き換える
 
-CHAT.fire = {
-  init:function(){
-    this.setParameters();
-    this.bindEvent();
-  },
+const messages = document.getElementById('messages');
+const messageInput = document.getElementById('messageInput');
+const sendButton = document.getElementById('sendButton');
 
-  setParameters:function(){
-    this.$name = $('#jsi-name');
-    this.$textArea = $('#jsi-msg');
-    this.$board = $('#jsi-board');
-    this.$button = $('#jsi-button');
+socket.onopen = (event) => {
+    console.log('Connected to the WebSocket server');
+};
 
-    //データベースと接続する。各自登録時に出たコードに書き換え。
-    this.chatDataStore = new Firebase('https://<各自>.firebaseio.com/');
-  },
+socket.onmessage = (event) => {
+    const message = JSON.parse(event.data);
+    displayMessage(message.text);
+};
 
-  bindEvent:function(){
-    var self = this;
-    this.$button.on('click',function(){
-      self.sendMsg();
-    });
+socket.onclose = (event) => {
+    console.log('Connection to the WebSocket server is closed');
+};
 
-    //DBの「talks」から取り出す
-    this.chatDataStore.child('talks').on('child_added',function(data){
-      var json = data.val();
-      self.addText(json['user']);
-      self.addText(json['message']);
-    });
-  },
-
-  //ユーザー、メッセージ送信
-  sendMsg:function(){
-    var self = this;
-    if (this.$textArea.val() == ''){ return }
-
-    var name = this.$name.val();
-    var text = this.$textArea.val();
-
-    //データベースの中の「talks」に値を送り格納（'talks'は各自任意に設定可能）
-    self.chatDataStore.child('talks').push({user:name, message:text});
-    self.$textArea.val('');
-  },
-
-  //受け取り後の処理
-  addText:function(json){
-   var msgDom = $('<li>');
-   msgDom.html(json);
-   this.$board.append(msgDom[0]);
-  }
-}
-
-$(function(){
-  CHAT.fire.init();
+sendButton.addEventListener('click', () => {
+    const messageText = messageInput.value;
+    if (messageText) {
+        const message = { text: messageText };
+        socket.send(JSON.stringify(message));
+        messageInput.value = '';
+    }
 });
+
+function displayMessage(text) {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = text;
+    messages.appendChild(messageElement);
+}
